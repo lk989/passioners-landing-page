bd44eaa7dfd47f4512716f3349b613b21dc43b0f
lk989 <lma.bugis@hotmail.com>
2024-08-19 16:48:57 +0300

    add social media icons in footer

---
+++ resources/views/components/social-media-icons.blade.php
@@ -0,0 +1,3 @@
+<a href="#" class="border border-gray rounded-circle p-2 d-flex align-items-center justify-content-center">
+    <img src="{{ asset('img/' . $iconPath . '') }}" height="{{ $height ?? '18rem' }}" alt="">
+</a>
