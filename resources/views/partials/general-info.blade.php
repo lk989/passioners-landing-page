56ac1a0cc9b37e50e2268a86b312764692a770c9
Lama <lma.bugis@hotmail.com>
2024-08-22 03:48:33 +0300

    make general card responsive

--- resources/views/partials/general-info.blade.php
+++ resources/views/partials/general-info.blade.php
@@ -1,6 +1,6 @@
-<section id="general-info" class="border border-dim-primary rounded-5 shadow-sm d-inline-flex p-5 w-100">
-    <img src="{{ asset('img/icon.png') }}" height="120rem" alt="">
-    <div class="mx-5 text-center mb-5">
+<section id="general-info" class="border border-dim-primary rounded-5 shadow-sm d-lg-inline-flex d-flex flex-column justify-content-center flex-lg-row justify-content-lg-start align-items-center align-items-lg-start p-5 w-100">
+    <img src="{{ asset('img/icon.png') }}" height="120rem" class="mb-4" alt="">
+    <div class="mx-lg-5 mx-3 text-center mb-lg-5 mb-3">
         <h2 class="tajawal-extrabold">Amal Mohammed</h2>
         <p class="text-black-50 lead inter-regular">Graphic Designer</p>
         <div class="d-inline-flex gap-3">
@@ -8,7 +8,7 @@
             <x-icon-label imgFile="location.png" label="16 مشروع"/>
         </div>
     </div>
-    <div>
+    <div class="d-none d-lg-block">
         <p class="badge bg-warning text-dark tajawal-bold">شغوف مميز <span><img src="{{ asset('img/check-broken.png') }}" height="18rem" alt=""></span></p>
     </div>
 </section>
